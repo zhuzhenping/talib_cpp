04816ffe8cf7d3cc12849f1ff3904424d88a1180
zhuzhenping <zhuzhenping@gmail.com>
2019-04-20 11:52:26 +0800

    Add files via upload

--- talib_test.cpp
+++ talib_test.cpp
@@ -14,19 +14,19 @@ int main()
     assert(retcode == TA_SUCCESS);
 
     // comput moving average price
-    TA_Real    close_price_array[400] = { 0 };
+    TA_Real    close_price_array[100] = { 0 };
     // construct random price
     srand((unsigned)time(0));
-    for (int i = 0; i < 400; i++)
+    for (int i = 0; i < 100; i++)
         close_price_array[i] = rand() % 50 + 100;
 
-    TA_Real    out[400] = { 0 };
-    TA_Integer out_begin = 0;
-    TA_Integer out_nb_element = 0;
+    TA_Real    out[100] = { 0 };
+    TA_Integer out_begin =0 ;
+    TA_Integer out_nb_element=0  ;
 
-    retcode = TA_MA(0, 399,
+    retcode = TA_MA(0, 99,
         &close_price_array[0],
-        30, TA_MAType_SMA,
+        3, TA_MAType_SMA,
         &out_begin, &out_nb_element, &out[0]);
 
     assert(retcode == TA_SUCCESS);
@@ -35,8 +35,22 @@ int main()
     cout << "out_begin_index: " << out_begin << endl;
     cout << "out_nb_element: " << out_nb_element << endl;
     cout << "out price array: " << endl;
+    
+    /* The output isdisplayed here */
+    int i;
+    for( i=0; i <out_nb_element; i++ )
+       printf( "%d = %f\n", out_begin+i,out[i] );
+   
+    printf( "%d = %f\n", out_begin+out_nb_element-1,out[out_nb_element-1]); /last record
+    
+    for (auto &price : close_price_array)
+        cout << price << " ";
+    cout<<endl<<endl;
+       
     for (auto &price : out)
-        cout << price << " "<<endl;
+        cout << price << " ";
+    
+    
 
     retcode = TA_Shutdown();
     assert(retcode == TA_SUCCESS);
