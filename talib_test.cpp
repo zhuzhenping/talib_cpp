c352a896dc7251e43bcf9315edceed744d4e17d0
zhuzhenping <zhuzhenping@gmail.com>
2019-04-20 13:34:06 +0800

    Add files via upload

--- talib_test.cpp
+++ talib_test.cpp
@@ -2,10 +2,24 @@
 #include <time.h>
 #include <assert.h>
 #include "ta_libc.h"
+#include <deque>
+
 using namespace std;
 
 int main()
 {
+	//
+	deque<double> price;
+	int look_back=20;
+	price.clear();
+	for (int i = 0; i < 100; i++){
+	    price.push_back(rand() % 50 + 100);
+	    if (price.size() > look_back)
+            price.pop_front();
+	    
+	}
+	//
+            
     cout << "ta-lib test" << endl;
 
     // init ta-lib context
@@ -24,11 +38,19 @@ int main()
     TA_Integer out_begin =0 ;
     TA_Integer out_nb_element=0  ;
 
+/*
     retcode = TA_MA(0, 99,
         &close_price_array[0],
         3, TA_MAType_SMA,
         &out_begin, &out_nb_element, &out[0]);
 
+
+ */ 
+    retcode = TA_MA(0, 20,
+        &price[0],
+        3, TA_MAType_SMA,
+        &out_begin, &out_nb_element, &out[0]);
+      
     assert(retcode == TA_SUCCESS);
 
     cout << "---- compute output ----" << endl;
@@ -38,18 +60,20 @@ int main()
     
     /* The output isdisplayed here */
     int i;
+    
+    for (auto &price : price)
+        cout << price << " ";
+    cout<<endl<<endl;
+    
     for( i=0; i <out_nb_element; i++ )
        printf( "%d = %f\n", out_begin+i,out[i] );
    
-    printf( "%d = %f\n", out_begin+out_nb_element-1,out[out_nb_element-1]); //last record
+    printf( "最后一条记录：%d = %f\n", out_begin+out_nb_element-1,out[out_nb_element-1]); //last record       
     
-    for (auto &price : close_price_array)
-        cout << price << " ";
-    cout<<endl<<endl;
-       
+       /*
     for (auto &price : out)
         cout << price << " ";
-    
+    */
     
 
     retcode = TA_Shutdown();
