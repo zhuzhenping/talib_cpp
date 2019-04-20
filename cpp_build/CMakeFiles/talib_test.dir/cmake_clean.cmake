7ee89e61b004741d0077b5156c56b7916a50bc3f
zhuzhenping <zhuzhenping@gmail.com>
2019-04-20 11:05:50 +0800

    Add files via upload

---
+++ cpp_build/CMakeFiles/talib_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talib_test.dir/talib_test.cpp.o"
+  "talib_test.pdb"
+  "talib_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talib_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
